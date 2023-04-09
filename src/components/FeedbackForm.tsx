db730a59e9becab63d62da06b91c1c1fc5645002
TacitusPie <kakmsaikou@gmail.com>
2023-04-09 21:09:04 +0800

    feat: 根据输入的不同呈现 text 和 btnDisabled

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -4,7 +4,21 @@ import Button from './shared/Button';
 
 const FeedbackForm = () => {
   const [text, setText] = useState('');
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState('');
+
   const handleTextChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value === '') {
+      setMessage('');
+      setBtnDisabled(true);
+    } else if (e.target.value.trim().length <= 10) {
+      setMessage('Text must be at least 10 characters');
+      setBtnDisabled(true);
+    } else {
+      setMessage('');
+      setBtnDisabled(false);
+    }
+
     setText(e.target.value);
   };
   return (
@@ -18,8 +32,11 @@ const FeedbackForm = () => {
             type='text'
             placeholder='Write a review'
           />
-          <Button>Send</Button>
+          <Button type='submit' isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
+        {message && <div className='message'>{message}</div>}
       </form>
     </Card>
   );
