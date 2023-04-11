a27393ac45658ce61752d6759820a33126eb1492
TacitusPie <kakmsaikou@gmail.com>
2023-04-11 14:12:40 +0800

    feat: 引入 react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import FeedbackDataList from './data/FeedbackData';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStatus from './components/FeedbackStatus';
 import FeedbackForm from './components/FeedbackForm';
+import AboutPage from './pages/AboutPage';
 
 const App = () => {
   const [feedbackDataList, setFeedbackDataList] = useState(FeedbackDataList);
@@ -18,14 +20,26 @@ const App = () => {
   return (
     <>
       <Header />
-      <div className='container'>
-        <FeedbackForm addFeedback={addFeedback} />
-        <FeedbackStatus feedbackDataList={feedbackDataList} />
-        <FeedbackList
-          feedbackDataList={feedbackDataList}
-          handleDelete={deleteFeedback}
-        />
-      </div>
+      <Router>
+        <div className='container'>
+          <Routes>
+            <Route
+              path='/'
+              element={
+                <>
+                  <FeedbackForm addFeedback={addFeedback} />
+                  <FeedbackStatus feedbackDataList={feedbackDataList} />
+                  <FeedbackList
+                    feedbackDataList={feedbackDataList}
+                    handleDelete={deleteFeedback}
+                  />
+                </>
+              }
+            />
+            <Route path='/about' element={<AboutPage />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 };
